442947d72d5ee8eb43e2ce894c6d7de9186b4def
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-11 02:07:09 -0300

    global state api

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import ArticleService from '../services/article_service'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading: false,
   },
   mutations: {
+    setLoading(state, payload) {
+      state.loading = payload;
+    }
   },
   actions: {
-  },
-  modules: {
+    getArticles({ commit }) {
+      return new Promise((resolve, reject) => {
+        commit('setLoading', true);
+        ArticleService.getArticles()
+          .then(response => {
+            commit('setLoading', false);
+            resolve(response);
+          })
+          .catch(error => {
+            commit('setLoading', false);
+            reject(error);
+          });
+      });
+    },
   }
 })
